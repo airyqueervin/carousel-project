e3a15e52b49a67f545f3026e985056a614832573
Airyque Ervin <airyqueervin@Airyque-Ervins-MacBook-Air.local>
2018-02-18 16:59:22 -0800

    Added in a placeholder image. Added padding on top of page.

--- src/client/components/MediaListEntry.jsx
+++ src/client/components/MediaListEntry.jsx
@@ -29,7 +29,7 @@ class MediaListEntry extends Component {
       <div className="slideSpace mySlides media-container" style={{flexDirection: "row"}}>
         { this.props.item.rating === null ? <i className="material-icons" onClick={this.handleLike}>favorite</i> : null }
         { this.props.item.itemData.youtube_video ? <i className="material-icons play" onClick={() => window.open(`https://www.youtube.com/watch?v=${this.props.item.itemData.youtube_video}`)}>play_circle_outline</i> : null }
-        <img src={this.props.item.itemData.image} style={{width: "224px", height: "283px", marginBottom: "24px", boxShadow:"10px 10px 50px black"}} />
+        <img src={this.props.item.itemData.image || "http://realishracing.com/images/archive/5/5f/20120315120949%21Placeholder.jpg"} style={{width: "224px", height: "283px", marginBottom: "24px", boxShadow:"10px 10px 50px black"}} />
         <div className="text" style={{fontSize: "24px"}}>{this.props.item.name}</div>
         <div className="text" style={{fontSize: "18px"}}>{this.props.item.itemData.definingInfo}</div>
       </div>
